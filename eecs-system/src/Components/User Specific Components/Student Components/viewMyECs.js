b24df08fba2de654d223b898601499c8a929c222
darenagos <darenagos@gmail.com>
2024-03-27 17:19:21 +0000

    css formatting for student create EC

--- eecs-system/src/Components/User Specific Components/Student Components/viewMyECs.js
+++ eecs-system/src/Components/User Specific Components/Student Components/viewMyECs.js
@@ -1,37 +1,34 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
+import "../../../reset.css";
+import "./viewMyECs.css";
 
-
-const ecRegistry = require("../../../Managers/ECRegistry")
-const currentUserManager = require("../../../Managers/CurrentUserManager")
+const ecRegistry = require("../../../Managers/ECRegistry");
+const currentUserManager = require("../../../Managers/CurrentUserManager");
 
 function ViewMyECs() {
-  
-
-  function getMyEcs(){
+  function getMyEcs() {
     const allEcs = ecRegistry.getAllECs();
-    console.log("Current User id:", currentUserManager.getCurrent().id)
-    console.log(allEcs)
-    let myEcs = []
-    console.log("starting loop")
-    for (let i=0; i<allEcs.length; i++){
-      console.log(i)
-      if (allEcs[i].userID === currentUserManager.getCurrent().id){
-        myEcs.push(allEcs[i])
-      }
-      else
-      {
-        console.log(allEcs[i].userID)
+    console.log("Current User id:", currentUserManager.getCurrent().id);
+    console.log(allEcs);
+    let myEcs = [];
+    console.log("starting loop");
+    for (let i = 0; i < allEcs.length; i++) {
+      console.log(i);
+      if (allEcs[i].userID === currentUserManager.getCurrent().id) {
+        myEcs.push(allEcs[i]);
+      } else {
+        console.log(allEcs[i].userID);
       }
     }
-    console.log("end loop")
-    console.log(myEcs)
-    return myEcs; 
+    console.log("end loop");
+    console.log(myEcs);
+    return myEcs;
   }
 
   function updateContent(ec) {
     setTitle(ec.name + " : " + ec.module);
     setInfo(ec.title + " - " + ec.date);
-    setDetails(ec.details)
+    setDetails(ec.details);
   }
 
   const [title, setTitle] = useState("Select an EC");
@@ -41,14 +38,37 @@ function ViewMyECs() {
 
   return (
     <div>
-      <div id="side">
-    {myEcs.map((ec) => (<button onClick={() => updateContent(ec)}>{ec.name} - {ec.title}</button>))} </div>
-    <div id="title">
-        {title}</div>
-      <div id="bottom">
-        {info}<br></br>{details}<button>Close EC</button></div>
+      <div className="padding-grid">
+        <div className="grid-container">
+          <div className="grid-row-span-2">
+            <div className="box">
+              <a>List of ECs:</a>
+              <br></br>
+              {myEcs.map((ec) => (
+                <button
+                  className="ec-title-button-side"
+                  onClick={() => updateContent(ec)}
+                >
+                  {ec.name} - {ec.title}
+                </button>
+              ))}{" "}
+            </div>
+          </div>
+
+          <div className="ec-title grid-col-span-2 box">{title}</div>
+          <div className="ec-content grid-col-span-2 box">
+            {info}
+            <br></br>
+            {details}
+            <button className="close-ec-button grid-col-span-2">
+              Close EC
+            </button>
+          </div>
+          {/* <div>fds</div> */}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ViewMyECs
+export default ViewMyECs;
