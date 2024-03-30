301244c3b0608b09244e26d881205e4fe29d3ee5
ec22472 <ec22472@qmul.ac.uk>
2024-03-30 18:06:29 +0000

    services tab now are not hard coded

--- eecs-system/src/Components/Non User Specific Components/ServiceStatus/serviceStatus.js
+++ eecs-system/src/Components/Non User Specific Components/ServiceStatus/serviceStatus.js
@@ -5,96 +5,101 @@ import warning from './imgaes/warning.png'
 import none from './imgaes/remove.png'
 
 function ServiceStatus() {  
+
+
+  const data = [
+    {
+    Entry1:"entry 1a",
+    Entry2:"entry 2a",
+    status:"partial"
+  },
+  {
+    Entry1:"entry 1b",
+    Entry2:"entry 2b",
+    status:"full"
+  },
+  {
+    Entry1:"entry 1c",
+    Entry2:"entry 2c",
+    status:"nones"
+  },
+ 
+]
   return (
     <div id="container">
-      <div id="service_header">
+    <div id="service_header">
         <h2 id="service_h1">Service Status</h2>
       </div>
       <div id="modified">
         Last modified: 14th March, 2024 at 11:35am
       </div>
-      <div id="services">
+
+      <div id = "services">
         <table id="main_table">
-          <tr className="gray">
-            <td>
-              <table class="entry-container">
-                <tr class="entry1">IT Service Desk System</tr>
-                <tr class="entry2">Self-service portal, phone, FAQ</tr>
-              </table>            
+          
+      {data.map((service, index) => (
+        <div key={index}>
+          {index % 2 === 0 ? (
+            // even index
+            <tr className = "gray">
+              <td>
+                <table class = "entry-container">
+                  <tr class="entry1">{service.Entry1} - Odd Index</tr>
+                  <tr class="entry2">{service.Entry2} </tr>
+                </table>
+                </td>
+              <td>
+            {/* final img */}
+            {service.status === "full" ? (
+              <img src={checkImage} alt="check" style={{ width: '20px', height: '20px' }}/>
+            ) : service.status === "partial" ? (
+              <img src={warning} alt="check" style={{ width: '20px', height: '20px' }}/>
+            ) : (
+              <img src={none} alt="check" style={{ width: '20px', height: '20px' }}/>
+            )}
             </td>
-            <td><img src={checkImage} alt="check" style={{ width: '20px', height: '20px' }}/></td>
           </tr>
-          <tr>
-            <td>
-              <table class="entry-container">
-                <tr class="entry1">Library Systems</tr>
-                <tr class="entry2">Search and services</tr>
-              </table>            
+            
+          ) : (
+            // odd index
+            <tr>
+              <td>
+                <table class = "entry-container">
+                  <tr class="entry1">{service.Entry1} - Even Index</tr>
+                  <tr class="entry2">{service.Entry2} </tr>
+                </table>
+                </td>
+              <td>
+            {/* final img */}
+            {service.status === "full" ? (
+              <img src={checkImage} alt="check" style={{ width: '20px', height: '20px' }}/>
+            ) : service.status === "partial" ? (
+              <img src={warning} alt="check" style={{ width: '20px', height: '20px' }}/>
+            ) : (
+              <img src={none} alt="check" style={{ width: '20px', height: '20px' }}/>
+            )}
             </td>
-            <td><img src={checkImage} alt="check" style={{ width: '20px', height: '20px' }}/></td>
           </tr>
-          <tr className="gray">
-            <td>
-              <table class="entry-container">
-                <tr class="entry1">MySIS</tr>
-                <tr class="entry2">Student record system</tr>
-              </table>            
-            </td>
-            <td><img src={checkImage} alt="check" style={{ width: '20px', height: '20px' }}/></td>
-          </tr>
-          <tr>
-            <td>
-              <table class="entry-container">
-                <tr class="entry1">MyTimetable</tr>
-                <tr class="entry2">Student timetables, room bookings</tr>
-              </table>            
-            </td>
-            <td><img src={checkImage} alt="check" style={{ width: '20px', height: '20px' }}/></td>
-          </tr>
-          <tr className="gray">
-            <td>
-              <table class="entry-container">
-                <tr class="entry1">Printing</tr>
-                <tr class="entry2">Central Print Service, Student Printing</tr>
-              </table>            
-            </td>
-            <td><img src={checkImage} alt="check" style={{ width: '20px', height: '20px' }}/></td>
-          </tr>
-          <tr>
-            <td>
-              <table class="entry-container">
-                <tr class="entry1">Remote Access</tr>
-                <tr class="entry2"></tr>
-              </table>            
-            </td>
-            <td><img src={checkImage} alt="check" style={{ width: '20px', height: '20px' }}/></td>
-          </tr>
-          <tr className="gray">
-            <td>
-              <table class="entry-container">
-                <tr class="entry1">Telephony</tr>
-                <tr class="entry2">Voicemail, staff telephones</tr>
-              </table>            
-            </td>
-            <td><img src={checkImage} alt="check" style={{ width: '20px', height: '20px' }}/></td>
-          </tr>
-          <tr>
-            <td>
-              <table class="entry-container">
-                <tr class="entry1">Wi-Fi</tr>
-                <tr class="entry2">eduroam</tr>
-              </table>            
-            </td>
-            <td><img src={checkImage} alt="check" style={{ width: '20px', height: '20px' }}/></td>
-          </tr>
-        </table>
-      </div>
+            
+          )}
+
+          
+        </div>
+      ))}
+      </table>
+
+      {/* end of table */}
+
       <div id="footer">
         <div id="good"><img src={checkImage} alt="check" style={{ width: '20px', height: '20px' }}/> Good Service</div>
         <div id="partial"><img src={warning} alt="check" style={{ width: '20px', height: '20px' }}/> Partial Service</div>
         <div id="none"><img src={none} alt="check" style={{ width: '20px', height: '20px' }}/> No Service</div>
-      </div>
+      </div> 
     </div>
+    
+ 
+    </div>
+    
   );
 }
 
