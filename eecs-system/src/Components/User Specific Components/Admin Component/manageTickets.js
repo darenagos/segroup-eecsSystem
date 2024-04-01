c6425a7a9d3cf2d671d91426dde979a51ab2779c
rApherC <55046271+rApherC@users.noreply.github.com>
2024-04-01 18:11:29 +0100

    manageTicket page/function

--- eecs-system/src/Components/User Specific Components/Admin Component/manageTickets.js
+++ eecs-system/src/Components/User Specific Components/Admin Component/manageTickets.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import "./admin.css";
 
-import ticketRegistry from "../../../Managers/TicketRegistry"
+import ticketRegistry from "../../../Managers/TicketRegistry";
 
 function ManageTickets() {
   function updateContent(ticket) {
-    setTitle(ticket.user.name + " : " + ticket.title);
-    setInfo(ticket.type + " - " + ticket.date);
+    setTitle(ticket.user.name + " (" + ticket.user.id + ") : " + ticket.type);
+    setInfo(ticket.title + " - " + ticket.date);
     setDetails(ticket.details);
   }
 
@@ -18,10 +18,37 @@ function ManageTickets() {
     return tickets;
   }
 
-  const [title, setTitle] = useState("Select an EC");
-  const [info, setInfo] = useState("");
-  const [details, setDetails] = useState();
+  function handleDelete(){
+    
+    let newCurrent
+    const currentIndex = ticketRegistry.getIndex(currentTicket)
+    console.log(currentIndex, " - ", ticketRegistry.getLength())
+    if (currentIndex == 0 && ticketRegistry.getLength()>1){
+      console.log("1")
+      newCurrent = ticketRegistry.getTicket(currentIndex+1)
+      
+    }
+    else if(currentIndex === ticketRegistry.getLength()-1){
+      console.log("delete final")
+      newCurrent = ticketRegistry.getTicket(currentIndex-1)
+    }
+    else{
+      console.log("other")
+      newCurrent = ticketRegistry.getTicket(ticketRegistry.getIndex(currentTicket) +1)
+    }
+    ticketRegistry.deleteEc(currentTicket)
+    updateContent(newCurrent)
+  }
+
+
+  const ticket0 = ticketRegistry.getTicket(1)
+  const [title, setTitle] = useState(ticket0.user.name + " (" + ticket0.user.id + ") : " + ticket0.module);
+  const [info, setInfo] = useState(ticket0.title + " - " + ticket0.date);
+  const [details, setDetails] = useState(ticket0.details);
+  const [currentTicket, setCurrentTicket] = useState(ticket0);
   const tickets = getAllTickets();
+  console.log(tickets)
+
 
   return (
     <div>
@@ -36,7 +63,7 @@ function ManageTickets() {
                   className="ec-title-button-side"
                   onClick={() => updateContent(ticket)}
                 >
-                  {ticket.user.name} - {ticket.title}
+                  {ticket.name} - {ticket.title}
                 </button>
               ))}
             </div>
@@ -46,7 +73,12 @@ function ManageTickets() {
           <div className="ec-content grid-col-span-2 box">
             {info}
             <br></br>
+            Ticket Request Details: <br></br>
             {details}
+            <input type="text" ></input>
+            <button  className="deleteButton"
+              onClick= {handleDelete}
+            >Delete Ticket</button>
           </div>
         </div>
       </div>
