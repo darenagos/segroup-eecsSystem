e23d609f05dfa953380cd2b696ea6d8e8215a66f
darenagos <darenagos@gmail.com>
2024-04-01 17:37:56 +0100

    faq CSS in progress

--- eecs-system/src/Components/User Specific Components/Technician Components/updateFAQ.js
+++ eecs-system/src/Components/User Specific Components/Technician Components/updateFAQ.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import "./updateFaq.css"
+import React, { useState } from "react";
+import "./updateFaq.css";
 import getFaqData from "../../Non User Specific Components/FAQs/faqData"; // Import the function
 
 function UpdateFAQ() {
-  const [faqData, setFaqData] = useState(getFaqData()); 
+  const [faqData, setFaqData] = useState(getFaqData());
   const [questionToRemove, setQuestionToRemove] = useState("");
   const [questionToUpdate, setquestionToUpdate] = useState("");
   const [updatedQuestion, setupdatedQuestion] = useState("");
@@ -13,25 +13,25 @@ function UpdateFAQ() {
 
   const handleAddFaq = (e) => {
     e.preventDefault();
-  
-    if (newQuestion !== "" && newAnswer !== "" ) {
+
+    if (newQuestion !== "" && newAnswer !== "") {
       const newFaq = {
         Question: newQuestion,
         Answer: newAnswer,
       };
-  
+
       const updatedFaqData = [...faqData, newFaq];
       setFaqData(updatedFaqData);
       setNewQuestion("");
       setNewAnswer("");
-  
+
       localStorage.setItem("faqData", JSON.stringify(updatedFaqData));
     }
   };
 
   const handleRemoveFaq = (e) => {
     e.preventDefault();
-  
+
     if (questionToRemove !== "") {
       const updatedFaqData = [...faqData];
       updatedFaqData.splice(questionToRemove, 1);
@@ -43,7 +43,11 @@ function UpdateFAQ() {
 
   const handleUpdateFAQ = (e) => {
     e.preventDefault();
-    if (questionToUpdate !== "" && updatedQuestion !== "" && updatedAnswer !== "" ) {
+    if (
+      questionToUpdate !== "" &&
+      updatedQuestion !== "" &&
+      updatedAnswer !== ""
+    ) {
       const updatedFaq = {
         Question: updatedQuestion,
         Answer: updatedAnswer,
@@ -58,69 +62,81 @@ function UpdateFAQ() {
       setquestionToUpdate("");
       localStorage.setItem("faqData", JSON.stringify(updatedFaqData));
     }
-  }
+  };
 
   return (
-    <div id="update_main">
-      <form id="add_form" onSubmit={handleAddFaq}>
-        <input
-          type="text"
-          placeholder="New Question"
-          value={newQuestion}
-          onChange={(e) => setNewQuestion(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="New Answer"
-          value={newAnswer}
-          onChange={(e) => setNewAnswer(e.target.value)}
-        />
-        <button type="submit" className='submit_buttons'>Add FAQ</button>
-      </form>
-      <br />
-      <form id="remove_form" onSubmit={handleRemoveFaq}>
-        <select
-          value={questionToRemove}
-          onChange={(e) => setQuestionToRemove(e.target.value)}
-          className='select_question'
-        >
-          <option value="">Select a question to remove</option>
-          {faqData.map((faq, index) => (
-            <option key={index} value={index}>
-              {faq.Question}
-            </option>
-          ))}
-        </select>
-        <button type="submit" className='submit_buttons'>Remove FAQ</button>
-      </form>
-      <br />
-      <form id="update_form" onSubmit={handleUpdateFAQ}>
-        <select
-          value={questionToUpdate}
-          onChange={(e) => setquestionToUpdate(e.target.value)}
-          className='select_question'
-        >
-          <option value="">Select a question to update</option>
-          {faqData.map((faq, index) => (
-            <option key={index} value={index}>
-              {faq.Question}
-            </option>
-          ))}
-        </select>
-        <input
-          type="text"
-          placeholder="Updated Question"
-          value={updatedQuestion}
-          onChange={(e) => setupdatedQuestion(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Updated Answer"
-          value={updatedAnswer}
-          onChange={(e) => setupdatedAnswer(e.target.value)}
-        />
-        <button type="submit" className='submit_buttons'>Update FAQ</button>
-      </form>
+    <div className="grid-container-technician">
+      <div className="box-techician">
+        <form id="add_form" onSubmit={handleAddFaq}>
+          <input
+            className="input"
+            type="text"
+            placeholder="New Question"
+            value={newQuestion}
+            onChange={(e) => setNewQuestion(e.target.value)}
+          />
+          <input
+            className="input"
+            type="text"
+            placeholder="New Answer"
+            value={newAnswer}
+            onChange={(e) => setNewAnswer(e.target.value)}
+          />
+          <button type="submit" className="submit_buttons">
+            Add FAQ
+          </button>
+        </form>
+        <br />
+        <form id="remove_form" onSubmit={handleRemoveFaq}>
+          <select
+            value={questionToRemove}
+            onChange={(e) => setQuestionToRemove(e.target.value)}
+            className="select_question"
+          >
+            <option value="">Select a question to remove</option>
+            {faqData.map((faq, index) => (
+              <option key={index} value={index}>
+                {faq.Question}
+              </option>
+            ))}
+          </select>
+          <button type="submit" className="submit_buttons">
+            Remove FAQ
+          </button>
+        </form>
+        <br />
+        <form id="update_form" onSubmit={handleUpdateFAQ}>
+          <select
+            value={questionToUpdate}
+            onChange={(e) => setquestionToUpdate(e.target.value)}
+            className="select_question"
+          >
+            <option value="">Select a question to update</option>
+            {faqData.map((faq, index) => (
+              <option key={index} value={index}>
+                {faq.Question}
+              </option>
+            ))}
+          </select>
+          <input
+            className="input"
+            type="text"
+            placeholder="Updated Question"
+            value={updatedQuestion}
+            onChange={(e) => setupdatedQuestion(e.target.value)}
+          />
+          <input
+            className="input"
+            type="text"
+            placeholder="Updated Answer"
+            value={updatedAnswer}
+            onChange={(e) => setupdatedAnswer(e.target.value)}
+          />
+          <button type="submit" className="submit_buttons">
+            Update FAQ
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
