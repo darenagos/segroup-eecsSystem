1165684d10fa31104e83f1c949e13658cc7b7ae5
Saadi <ec22914@qmul.ac.uk>
2024-04-01 15:33:51 +0100

    These are all the changes to update the FAQ page accordingly

--- eecs-system/src/Components/Non User Specific Components/FAQs/Faqs.js
+++ eecs-system/src/Components/Non User Specific Components/FAQs/Faqs.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { useState } from "react";
-
+import React, { useState } from "react";
 import "./Faqs.css";
+import getFaqData from "./faqData"; 
 
 function Faqs() {
   const [selected, setSelected] = useState(null);
+  const faqData = getFaqData(); 
 
   const toggle = (i) => {
     if (selected === i) {
@@ -13,29 +13,11 @@ function Faqs() {
     setSelected(i);
   };
 
-  const data = [
-    {
-      Question: "Question 1",
-      Answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vulputate pellentesque suscipit. Proin a nisi id nisl dignissim pharetra eu ut eros. Sed cursus nisi arcu, ac imperdiet sem auctor in. Proin sed lacus ac diam molestie commodo. Donec eget quam vitae mauris facilisis vehicula. Integer ullamcorper ligula at augue molestie, molestie tempor nibh molestie. Mauris aliquet felis elit, at accumsan tellus maximus eget.",
-    },
-    {
-      Question: "Question 2",
-      Answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vulputate pellentesque suscipit. Proin a nisi id nisl dignissim pharetra eu ut eros. Sed cursus nisi arcu, ac imperdiet sem auctor in. Proin sed lacus ac diam molestie commodo. Donec eget quam vitae mauris facilisis vehicula. Integer ullamcorper ligula at augue molestie, molestie tempor nibh molestie. Mauris aliquet felis elit, at accumsan tellus maximus eget.",
-    },
-    {
-      Question: "Question 3",
-      Answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vulputate pellentesque suscipit. Proin a nisi id nisl dignissim pharetra eu ut eros. Sed cursus nisi arcu, ac imperdiet sem auctor in. Proin sed lacus ac diam molestie commodo. Donec eget quam vitae mauris facilisis vehicula. Integer ullamcorper ligula at augue molestie, molestie tempor nibh molestie. Mauris aliquet felis elit, at accumsan tellus maximus eget.",
-    },
-  ];
-
   return (
     <div className="container">
       <div className="">
         <h1 className="header">Frequently Asked Questions</h1>
-        {data.map((item, i) => (
+        {faqData.map((item, i) => (
           <div className="item" key={i}>
             <div className="title" onClick={() => toggle(i)}>
               <h2>{item.Question}</h2>
