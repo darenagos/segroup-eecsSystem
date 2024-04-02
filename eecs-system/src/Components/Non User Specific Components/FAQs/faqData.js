e037c5fcf575a31a8bbfea76732128c97aeef7c2
Saadi <ec22914@qmul.ac.uk>
2024-04-02 02:49:52 +0100

    added some default FAQ questions

--- eecs-system/src/Components/Non User Specific Components/FAQs/faqData.js
+++ eecs-system/src/Components/Non User Specific Components/FAQs/faqData.js
@@ -1,4 +1,16 @@
 const faqData = [
+    {
+        Question: "How do I login?",
+        Answer: "Use your username and password",
+    },
+    {
+        Question: "How do I report a problem",
+        Answer: "Submit a ticket",
+    },
+    {
+        Question: "How many self-certified ec's do I get?",
+        Answer: "You get 3 per academic year",
+    },
 ];
 
 const getFaqData = () => {
